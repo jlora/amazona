71f210d1286141e95be5697e61114ac87c6ffa54
JESUS ANDRES LORA CAMACHO <andres@submarine.app>
2020-12-18 09:11:08 -0700

    Implemented Shipping Address Screen and the information was saved into localstorage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigninScreen from './screens/SigninScreen';
 
 const App = () => {
@@ -48,6 +49,7 @@ const App = () => {
           <Route path='/' component={HomeScreen} exact />
           <Route path='/signin' component={SigninScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/shipping' component={ShippingAddressScreen}/>
           <Route path='/product/:id' component={ProductScreen} />
         </main>
         <footer className="row center">All right reserved</footer>
